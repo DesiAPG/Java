287e4f513b35be79f14a5da2dfe71ffb041c89bf
DesiAPG <desireapgames@gmail.com>
2022-09-02 10:54:21 -0500

    Class 2 : Aritmethics

---
+++ src/Aritmethics.java
@@ -0,0 +1,33 @@
+public class Aritmethics {
+
+    /* Clase 2
+       Aritmetica
+     */
+
+    public static void main(String[] args) {
+        int number1, number2, result;
+        number1 = 10;
+        number2 = 20;
+
+        // Addition
+        result = number2 + number1;
+        System.out.println(number2 + "+" + number1 + " is: "+result);
+
+        // Substraction
+        result = number2 - number1;
+        System.out.println(number2 + "-" + number1 + " is: "+result);
+
+        // Multiplication
+        result = number2 * number1;
+        System.out.println(number2 + "*" + number1 + " is: "+result);
+
+        // Difference
+        result = number2 / number1;
+        System.out.println(number2 + "/" + number1 + " is: "+result);
+
+
+
+
+
+    }
+}
