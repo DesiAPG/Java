bc0234a2a7a2dafcd0820136a5995393395e7a42
DesiAPG <desireapgames@gmail.com>
2022-09-02 11:52:55 -0500

    Class 2 : ControlStructures

---
+++ src/ControlStructures.java
@@ -0,0 +1,18 @@
+public class ControlStructures {
+
+    /* Class 2
+       ControlStructures
+     */
+
+    public static void main(String[] args) {
+        int number = 212;
+
+        if (number % 2 == 0) {
+            System.out.println("Is Pair");
+        } else {
+            System.out.println("Is odd");
+        }
+        System.out.println("Value: " + number);
+
+    }
+}
